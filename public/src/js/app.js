5d43b3ed18180cc97f2be508ebfe422d8d960a6d
Pravin Zade <skyrites@gmail.com>
2020-02-29 20:44:32 +0530

    cleanup project

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -16,88 +16,88 @@ if ('serviceWorker' in navigator) {
 }
 
 window.addEventListener('beforeinstallprompt', function(event) {
-  console.log('beforeinstallprompt fired');
+  // console.log('beforeinstallprompt fired');
   event.preventDefault();
   deferredPrompt = event;
   return false;
 });
 
-var promise = new Promise(function(resolve, reject) {
-  setTimeout(() => {
-    // resolve('This is executed once the timer is done!');
-    reject({ code: 500, message: 'An error occured!' });
-    // console.log('This is executed once the timer is done!');
-  }, 3000);
-});
+// var promise = new Promise(function(resolve, reject) {
+//   setTimeout(() => {
+//     // resolve('This is executed once the timer is done!');
+//     reject({ code: 500, message: 'An error occured!' });
+//     // console.log('This is executed once the timer is done!');
+//   }, 3000);
+// });
 
-// ------------------- AJAX Query for data fetch
-// var xhr = new XMLHttpRequest();
-// xhr.open('GET', 'https://httpbin.org/ip');
-// xhr.responseType = 'json';
+// // ------------------- AJAX Query for data fetch
+// // var xhr = new XMLHttpRequest();
+// // xhr.open('GET', 'https://httpbin.org/ip');
+// // xhr.responseType = 'json';
 
-// xhr.onload = function() {
-//   console.log(xhr.response);
-// };
-// xhr.onerror = function() {
-//   console.log('Error!');
-// };
-// xhr.send();
-// ---------------- AJAX Query for data fetch end
+// // xhr.onload = function() {
+// //   console.log(xhr.response);
+// // };
+// // xhr.onerror = function() {
+// //   console.log('Error!');
+// // };
+// // xhr.send();
+// // ---------------- AJAX Query for data fetch end
 
-fetch('https://httpbin.org/ip')
-  .then(function(response) {
-    console.log(response);
-    return response.json();
-  })
-  .then(function(data) {
-    console.log(data);
-  })
-  .catch(function(err) {
-    console.log(err);
-  });
-fetch('https://httpbin.org/post', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-    Accept: 'application/json'
-  },
-  // mode: 'no-cors',
-  mode: 'cors',
-  body: JSON.stringify({
-    message: 'Does this work?'
-  })
-})
-  .then(function(response) {
-    console.log(response);
-    return response.json();
-  })
-  .then(function(data) {
-    console.log(data);
-  })
-  .catch(function(err) {
-    console.log(err);
-  });
+// fetch('https://httpbin.org/ip')
+//   .then(function(response) {
+//     console.log(response);
+//     return response.json();
+//   })
+//   .then(function(data) {
+//     console.log(data);
+//   })
+//   .catch(function(err) {
+//     console.log(err);
+//   });
+// fetch('https://httpbin.org/post', {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json',
+//     Accept: 'application/json'
+//   },
+//   // mode: 'no-cors',
+//   mode: 'cors',
+//   body: JSON.stringify({
+//     message: 'Does this work?'
+//   })
+// })
+//   .then(function(response) {
+//     console.log(response);
+//     return response.json();
+//   })
+//   .then(function(data) {
+//     console.log(data);
+//   })
+//   .catch(function(err) {
+//     console.log(err);
+//   });
+// // promise
+// //   .then(
+// //     function(text) {
+// //       // console.log(text);
+// //       return text;
+// //     },
+// //     function(err) {
+// //       console.log(err.code, err.message);
+// //     }
+// //   )
+// //   .then(function(newText) {
+// //     console.log(newText);
+// //   });
 // promise
-//   .then(
-//     function(text) {
-//       // console.log(text);
-//       return text;
-//     },
-//     function(err) {
-//       console.log(err.code, err.message);
-//     }
-//   )
+//   .then(function(text) {
+//     return text;
+//   })
 //   .then(function(newText) {
 //     console.log(newText);
+//   })
+//   .catch(function(err) {
+//     console.log(err.code, err.message);
 //   });
-promise
-  .then(function(text) {
-    return text;
-  })
-  .then(function(newText) {
-    console.log(newText);
-  })
-  .catch(function(err) {
-    console.log(err.code, err.message);
-  });
-console.log('This is executed right after SetTimeout()');
+// console.log('This is executed right after SetTimeout()');
--- public/sw.js
+++ public/sw.js
@@ -3,11 +3,11 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('activate', function(event) {
-  console.log('[Service Worker] Activating Service Worker ...', event);
+  console.log('[Service Worker] Activating Service Worker ....', event);
   return self.clients.claim();
 });
 
 self.addEventListener('fetch', function(event) {
-  console.log('[Service Worker] Fetching something ....', event);
+  // console.log('[Service Worker] Fetching something ....', event);
   event.respondWith(fetch(event.request));
 });
